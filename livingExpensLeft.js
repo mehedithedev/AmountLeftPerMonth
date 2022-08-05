3f7de81ec03b13c6b4d474ebd118718a50ff9e65
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-08-05 18:55:08 +0600

    Update livingExpensLeft.js

--- livingExpensLeft.js
+++ livingExpensLeft.js
@@ -1,16 +1,17 @@
 let tuitionFee= prompt(`What's your tuition fee per Year? `)
 let hourlyRate= prompt(`How much do you earn in an hour? `)
 let yourStrength= prompt(`How many hours you can work when you are working full time in a day? `)
+let familySupport= prompt(`How much money you can expect from during your studies for a year?`)
 
-let partTime=(hourlyRate*4*5*4)*8
-let fullTime=(hourlyRate*yourStrength*5*4)*4
-let netWorth= partTime+fullTime
+let partTime=(hourlyRate*4*5*4)*8;
+let fullTime=(hourlyRate*yourStrength*5*4)*4;
+let netWorth= partTime+fullTime+familySupport;
 
-let ownCost=(netWorth-tuitionFee)/12
+let ownCost=(netWorth-tuitionFee)/12;
 
 if (typeof ownCost === NaN) {
     document.write(`Enter valid enput`)
 } else {    
-    document.write(`There will left approximately ${Math.floor(ownCost)}$ , for your living expense and others for a month.`) 
+    document.write(`There will left approximately ${Math.floor(ownCost)}$ for your living expense and others for a month.`) 
 }
 console.log(typeof ownCost)
